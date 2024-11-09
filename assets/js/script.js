67f8f7edcee7b1cfc39a24caa293eca461a066ad
Jose Carmona <josshue85@gmail.com>
2024-11-09 11:45:22 -0300

    Iniciado Desafío 4 - Inmobiliaria (Arreglos), creada la estructura de carpeta del proyecto con los archivos creados. Se trabaja en la página propiedades_alquile.html

---
+++ assets/js/script.js
@@ -0,0 +1,98 @@
+/* **************** PROPIEDADES EN ALQUILER **************** */
+// Arreglo de objetos
+const propiedadesAlquiler = [
+    {
+        nombre: 'Casa en el campo',
+        src: '',
+        descripcion: 'Cómoda casa ubicada a las afueras de la ciudad en terreno llano rodeada de árboles frutales',
+        ubicacion: '635 Camino el Ruedo, Hualqui',
+        habitaciones: 4,
+        baños: 2,
+        costo: 6500,
+        smoke: false,
+        pets: true
+    },
+    {
+        nombre: 'Departamento en el centro de la ciudad',
+        src: '',
+        descripcion: 'Hermoso departamento ubicado en la zona céntrica de la ciudad, cerca de universidades, comercio, hospitales, etc.',
+        ubicacion: '2466 José Fernández, Concepción',
+        habitaciones: 2,
+        baños: 1,
+        costo: 5000,
+        smoke: false,
+        pets: false
+    },
+    {
+        nombre: 'Casa en zona residencial',
+        src: '',
+        descripcion: 'Amplia casa ubicada en zona residencial, muy tranquila',
+        ubicacion: '792 Río Bueno, Talcahuano',
+        habitaciones: 6,
+        baños: 3,
+        costo: 8000,
+        smoke: true,
+        pets: true
+    },
+    {
+        nombre: 'Casa en la playa',
+        src: '',
+        descripcion: 'Casa ubicada justo a orilla de la playa',
+        ubicacion: '5329 Puig Jara, Dichato',
+        habitaciones: 4,
+        baños: 2,
+        costo: 10000,
+        smoke: false,
+        pets: false
+    },
+    {
+        nombre: 'Departamento alejado del centro de la ciudad',
+        src: '',
+        descripcion: 'Acogedor departamento ubicado en zona residecial extremadamente tranquila, alejado del ritmo frenetico que se vive en el centro de la ciudad',
+        ubicacion: '6873 Media Plaza, Concepción',
+        habitaciones: 3,
+        baños: 2,
+        costo: 3500,
+        smoke: false,
+        pets: false
+    },
+    {
+        nombre: 'Condominio en zona suburbana',
+        src: '',
+        descripcion: 'Ubicado en las afueras de la ciudad donde se respira aire fresco y reina la paz y la tranquilidad, con servicio de vigilancia 24/7',
+        ubicacion: '513 La Verdadera Luz, Chiguayante',
+        habitaciones: 5,
+        baños: 3,
+        costo: 7600,
+        smoke: true,
+        pets: true
+    },
+    {
+        nombre: 'Casa en el campo',
+        src: '',
+        descripcion: 'Rodeada de viñedos se ubica esta preciosa casa donde impera la paz',
+        ubicacion: '145 Redondo García, Cabrero',
+        habitaciones: 7,
+        baños: 2,
+        costo: 10500,
+        smoke: true,
+        pets: true
+    },
+    {
+        nombre: 'Casa en zona residencial',
+        src: '',
+        descripcion: 'Casa ubicada entre el centro y las afueras de la ciudad permitiendo estar en una zona más tranquila sin tener que alejarse tanto de la ciudad',
+        ubicacion: '3576 Sol Vive, Concepción',
+        habitaciones: 4,
+        baños: 2,
+        costo: 6200,
+        smoke: false,
+        pets: true
+    }
+]
+
+/*********************************************************** */
+
+/* **************** PROPIEDADES EN VENTA **************** */
+
+/******************************************************** */
